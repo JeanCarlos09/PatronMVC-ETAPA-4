3afef822d1407c7d69846ef8f8fe908ab728ae17
JeanCarlos09 <136395052+JeanCarlos09@users.noreply.github.com>
2023-07-19 21:55:57 -0500

    Usuario.cs

--- Usuario.cs
+++ Usuario.cs
@@ -1,10 +1,21 @@
 // Modelo: representa los datos y la lógica de negocio
 public class Usuario
 {
+    // Propiedad para almacenar el nombre del usuario.
     public string Nombre { get; set; }
+
+    // Propiedad para almacenar la edad del usuario.
     public int Edad { get; set; }
+
+    // Propiedad para almacenar la cédula del usuario.
     public string Cedula { get; set; }
+
+    // Propiedad para almacenar el número de celular del us
     public string Celular { get; set; }
+
+    // Propiedad para almacenar el correo electrónico del usuario.
     public string Correo { get; set; }
+
+    // Propiedad para almacenar la contraseña del usuario.
     public string Contrasena { get; set; }
 }
