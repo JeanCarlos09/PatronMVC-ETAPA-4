fd8c6358afa8c985a5752c565e6de49d2bd2db02
JeanCarlos09 <136395052+JeanCarlos09@users.noreply.github.com>
2023-07-19 21:46:03 -0500

    AplicacionEnvioPaquetes.cs

--- AplicacionEnvioPaquetes.cs
+++ AplicacionEnvioPaquetes.cs
@@ -4,16 +4,25 @@ public class AplicacionEnvioPaquetes
 {
     private ControladorUsuario controlador;
 
+    // Constructor que inicializa el objeto del controlador de usuario.
     public AplicacionEnvioPaquetes()
     {
         controlador = new ControladorUsuario();
     }
 
+    // Método "Ejecutar" que realiza una secuencia de acciones relacionadas con el controlador de usuario.
     public void Ejecutar()
     {
+        // Registrar un nuevo usuario.
         controlador.RegistrarUsuario();
+
+        // Iniciar sesión para poder realizar acciones adicionales.
         controlador.IniciarSesion();
+
+        // Envía un paquete (solo si el usuario ha iniciado sesión correctamente).
         controlador.EnviarPaquete();
+
+        // Cierra la sesión del usuario actual.
         controlador.CerrarSesion();
     }
 }
